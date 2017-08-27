489f3e0835544a973fad09d6bfe3a05af905435e
Chris Ovenden <>
2017-08-27 18:08:15 +0100

    cumulative return

--- fund/models.py
+++ fund/models.py
@@ -30,6 +30,19 @@ class Fund(models.Model):
         pds = pd.Series(percentages, dates)
         return pds
 
+    def get_cumulative_return(self):
+        time_data = self.returnitem_set.all()
+        dates = [td.date for td in time_data]
+        prev_value = 1
+        cumulative_data = []
+        for td in time_data:
+            next_value = prev_value * (1 + (td.percentage/100))
+            cumulative_data.append(next_value)
+            prev_value = next_value
+
+        pds = pd.Series(cumulative_data, dates)
+        return pds
+
     def add_return_item(self, date, percentage):
         self.returnitem_set.create(
             date=date,
@@ -41,7 +54,20 @@ class ReturnItem(models.Model):
     percentage = models.DecimalField(decimal_places=2, max_digits=5)
     date = models.DateField()
     fund = models.ForeignKey(Fund, on_delete=models.CASCADE)
+    #month = models.IntegerField()
+    #year = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        #self.month = self.date.month
+        #self.year = self.data.year
+        super(ReturnItem, self).save(*args, **kwargs)
+
+    # def prev(self):  # assumes one entry per month; how to enforce?
+    #     prev_month = self.date.month
+    #     ReturnItem.objects.get(fund=self.fund, )
 
     def __str__(self):
         return '%s%s on %s' % (self.percentage, '%', self.date.strftime('%d-%m-%Y'))
 
+    class Meta:
+        ordering = ['date']
